d38b4ae9ac12c2cf0dbe68e9cb7e0928cad45cbe
sxj <jimlinjun@qq.com>
2017-07-01 00:37:31 +0800

    调整获取接口

--- qt-financial-appserver/src/main/java/com/qtjf/appserver/controller/ProductController.java
+++ qt-financial-appserver/src/main/java/com/qtjf/appserver/controller/ProductController.java
@@ -14,29 +14,44 @@ import com.qtjf.common.vo.ResultCode;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
-	
+
 	@Autowired
 	ProductServer productserver;
 	
 	/**
 	 * 获取所有产品
-	 * @param userId 用户Id
-	 * @return 产品集合
+	 * @return
 	 */
-    @RequestMapping(method = RequestMethod.GET)
-	public ResultCode getProducts(String userId) {
-    		
-    		ResultCode result = null;
-    		try {
-    			List<QtFinacialProduct> list = productserver.getProducts(userId);
-    			result = ResultCode.getSuccess("获取产品成功", list.size(), list);
+	@RequestMapping(method = RequestMethod.GET)
+	public ResultCode getProducts() {
+		ResultCode result = null;
+		try {
+			List<QtFinacialProduct> list = productserver.getProducts("");
+			result = ResultCode.getSuccess("获取所有产品成功", list.size(), list);
 		} catch (Exception e) {
-			result = ResultCode.getFail("获取产品失败");
+			result = ResultCode.getFail("获取所有产品失败");
 			e.printStackTrace();
 		}
 		return result;
 	}
-	
-	
+
+	/**
+	 * 获取所有产品及用户信息(次数，是否可以申请)
+	 * @param userId 用户Id
+	 * @return 产品集合
+	 */
+	@RequestMapping(value = "/{userId}", method = RequestMethod.GET)
+	public ResultCode getProducts(String userId) {
+		System.out.println("userId=" + userId);
+		ResultCode result = null;
+		try {
+			List<QtFinacialProduct> list = productserver.getProducts(userId);
+			result = ResultCode.getSuccess("获取用户产品成功", list.size(), list);
+		} catch (Exception e) {
+			result = ResultCode.getFail("获取用户产品失败");
+			e.printStackTrace();
+		}
+		return result;
+	}
 
 }
