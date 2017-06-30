fcd642665a157326b6d220ab6ad547bbd7c23454
sxj <jimlinjun@qq.com>
2017-06-30 14:32:33 +0800

    配置

--- qt-financial-appserver/src/main/java/com/qtjf/appserver/Application.java
+++ qt-financial-appserver/src/main/java/com/qtjf/appserver/Application.java
@@ -1,5 +1,6 @@
 package com.qtjf.appserver;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author shixianjie
  */
 @SpringBootApplication
+@MapperScan("com.qtjf.appserver.dao")
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
