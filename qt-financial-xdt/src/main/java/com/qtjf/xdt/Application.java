6f9286308495aea471c1eda5df5d3e39457b8cbf
sxj <jimlinjun@qq.com>
2017-07-04 16:43:39 +0800

    借贷通信贷流量业务对接接口

---
+++ qt-financial-xdt/src/main/java/com/qtjf/xdt/Application.java
@@ -0,0 +1,30 @@
+package com.qtjf.xdt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * spring启动类
+ * @author shixianjie
+ */
+@SpringBootApplication
+public class Application {
+	
+	@Autowired  
+    private RestTemplateBuilder builder;  
+  
+    // 使用RestTemplateBuilder来实例化RestTemplate对象，spring默认已经注入了RestTemplateBuilder实例  
+    @Bean  
+    public RestTemplate restTemplate() {  
+        return builder.build();  
+    }  
+    
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ qt-financial-xdt/src/main/java/com/qtjf/xdt/controller/CreditFlowController.java
@@ -0,0 +1,144 @@
+package com.qtjf.xdt.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CreditFlowController {
+
+	@Autowired
+	private RestTemplate template;
+
+	@Value("${jdt.testHost}")
+	private String host;
+
+	/**
+	 * 实时通告接口
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "Communication", method = RequestMethod.POST)
+	public String Communication(String token, String demandId, String dmandKey, String dataTime,
+			HttpServletRequest req) {
+
+		req.getSession().setAttribute("token", token);
+		req.getSession().setAttribute("dmandKey", dmandKey);
+		return "hi! " + token;
+	}
+
+	/**
+	 * accessToken获取接口
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "AccessToken", method = RequestMethod.GET)
+	public Map<String, String> AccessToken(String org, String secret, HttpServletRequest req) {
+		
+		req.getSession().setAttribute("org", org);
+		req.getSession().setAttribute("secret", secret);
+
+		String url = host + "AccessToken?org=" + org + "&secret=" + secret;
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = template.getForObject(url, Map.class);
+
+		if (Objects.equals(map.get("result"), "SUCCESS")) {
+			req.getSession().setAttribute("accessToken", map.get("accessToken"));
+		}
+
+		return map;
+	}
+
+	@RequestMapping(value = "GetSSPData", method = RequestMethod.GET)
+	public Map<String,Object> getSSPdata(HttpServletRequest req) {
+		
+		Map<String, String> map = getOrgInfo(req);
+
+		String url = host + "GetSSPData?org=" + map.get("orgName") + "&secret=" + map.get("secret")
+				+ "&accessToken=" + map.get("accessToken")+"&token="+ map.get("token");
+		
+		System.out.println(url);
+		@SuppressWarnings("unchecked")
+		Map<String, Object> result = template.getForObject(url, Map.class);
+
+		return result;
+	}
+
+	/**
+	 * 未拉取报告查询接口
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "QueryOustandingTokens", method = RequestMethod.GET)
+	public Map<String, Object> QueryOustandingTokens(HttpServletRequest req) {
+
+		Map<String, String> map = getOrgInfo(req);
+		String url = host + "QueryOustandingTokens?org=" + map.get("orgName") + "&secret=" + map.get("secret")
+				+ "&accessToken=" + map.get("accessToken");
+		
+		@SuppressWarnings("unchecked")
+		Map<String, Object> result = template.getForObject(url, Map.class);
+
+		return result;
+	}
+
+	/**
+	 * 商户3日流量查询接口
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "QueryTokensByDemand", method = RequestMethod.GET)
+	public Map<String, Object> QueryTokensByDemand(HttpServletRequest req) {
+
+		Map<String, String> map = getOrgInfo(req);
+
+		String url = host + "QueryTokensByDemand??org=" + map.get("orgName") + "&secret=" + map.get("secret")
+				+ "&accessToken=" + map.get("accessToken") + "&demandKey=" + map.get("demandKey");
+
+		@SuppressWarnings("unchecked")
+		Map<String, Object> result = template.getForObject(url, Map.class);
+
+		return result;
+	}
+
+	/**
+	 * 账户下所有商户列表查询
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "QueryConsume", method = RequestMethod.GET)
+	public Map<String, Object> QueryConsume(HttpServletRequest req) {
+		
+		Map<String, String> map = getOrgInfo(req);
+		String url = host + "QueryConsume?org=" + map.get("orgName") + "&secret=" + map.get("secret") + "&accessToken="
+				+ map.get("accessToken");
+
+		@SuppressWarnings("unchecked")
+		Map<String, Object> result = template.getForObject(url, Map.class);
+
+		return result;
+	}
+
+	private Map<String, String> getOrgInfo(HttpServletRequest req) {
+		
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("token", (String) req.getSession().getAttribute("token"));
+		map.put("orgName", (String) req.getSession().getAttribute("org"));
+		map.put("secret", (String) req.getSession().getAttribute("secret"));
+		map.put("accessToken", (String) req.getSession().getAttribute("accessToken"));
+		map.put("demandKey", (String) req.getSession().getAttribute("demandKey"));
+
+		return map;
+
+	}
+
+}
