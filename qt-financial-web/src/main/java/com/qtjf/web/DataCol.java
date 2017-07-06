6e02146e3888272d92c9fe38357542c5418965b5
Administrator <Administrator@XB-20161003WEZR>
2017-07-06 21:04:22 +0800

    改项目名

--- qt-financial-web/src/main/java/com/qtjf/web/DataCol.java
+++ qt-financial-web/src/main/java/com/qtjf/web/DataCol.java
@@ -1,10 +1,18 @@
 package com.qtjf.web;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value="data")
 public class DataCol {
-
+	
+	//分页获取审核数据，类型分审核中和审核完毕的
+	@RequestMapping(value = "/all")
+	public Map<String,Object> all(String type,Integer start,Integer end) {
+		return null;
+	}
+	
 }
