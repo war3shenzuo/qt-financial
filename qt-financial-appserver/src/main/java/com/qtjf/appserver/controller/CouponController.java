88ac64e780232a112cc204d34236c8e5ea810c38
Administrator <Administrator@PC-20160313MOEE>
2017-07-09 11:09:20 +0800

    创建优惠券接口

---
+++ qt-financial-appserver/src/main/java/com/qtjf/appserver/controller/CouponController.java
@@ -0,0 +1,15 @@
+package com.qtjf.appserver.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 优惠券Controller类
+ * @author 史贤杰
+ * 2017/07/01
+ */
+@RestController
+@RequestMapping("/coupons")
+public class CouponController {
+
+}
