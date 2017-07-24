47e23a8dd8f86228e26a4b98c9b8069878f50085
sxj <jimlinjun@qq.com>
2017-07-24 10:58:41 +0800

    banner地址调整

--- qt-financial-appserver/src/main/java/com/qtjf/appserver/server/impl/BannerServerImpl.java
+++ qt-financial-appserver/src/main/java/com/qtjf/appserver/server/impl/BannerServerImpl.java
@@ -3,6 +3,7 @@ package com.qtjf.appserver.server.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.qtjf.appserver.dao.QtFinanicalBannerMapper;
@@ -15,9 +16,17 @@ public class BannerServerImpl implements BannerServer {
 	@Autowired
 	QtFinanicalBannerMapper bannerDao;
 	
+	@Value(value = "picHost")
+	private String picHost;
+	
 	@Override
 	public List<QtFinanicalBanner> getBanner() {
-		return bannerDao.selectAll();
+		
+		List<QtFinanicalBanner> list = bannerDao.selectAll();
+		for(QtFinanicalBanner b :list){
+			b.setPic(picHost+b.getPic());
+		}
+		return list;
 	}
 
 }
