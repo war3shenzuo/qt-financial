8bd28cf56490d56d3c975b1e9e719e6807aa8c36
sxj <jimlinjun@qq.com>
2017-07-27 10:09:29 +0800

    打包问题

--- qt-financial-appserver/src/main/java/com/qtjf/appserver/Application.java
+++ qt-financial-appserver/src/main/java/com/qtjf/appserver/Application.java
@@ -1,17 +1,12 @@
 package com.qtjf.appserver;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 
 import com.qtjf.jwt.entity.JwtConfig;
-import com.qtjf.jwt.filter.HTTPBasicAuthorizeAttribute;
 
 /**
  * spring启动类
@@ -40,5 +35,5 @@ public class Application {
 // 		registrationBean.setUrlPatterns(urlPatterns);
 //		return registrationBean;
 //	}
-
+	
 }
